6d9d79eed012bda564e45a65457ba55acb145aeb
xopherdeep <heylisten@xtiv.net>
2016-07-03 15:50:09 -0700

    xAdvertise/ Automated Nightly Push

---
+++ xAdvertise.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @name Advertisers
+ * @desc 
+ * @version v0
+ * @author @xopherdeep
+ * @mini flag
+ * @see radius
+ * @link advertise
+ * @todo
+ * @alpha
+ */
+
+  class xAdvertise extends Xengine {
+
+  }
+?>
