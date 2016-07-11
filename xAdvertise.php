b9b93775f8b2db653a1df3c378e30ba953ca3a90
xopherdeep <heylisten@xtiv.net>
2016-07-10 20:22:25 -0700

    xAdvertise/ Automated Nightly Push

--- xAdvertise.php
+++ xAdvertise.php
@@ -2,7 +2,7 @@
 /**
  * @name    Advertisers
  * @desc    Manage your advertisements
- * @version v0.1
+ * @version v0.1(160710)
  * @author  @xopherdeep
  * @mini    bar-chart-o 
  * @icon    test.png
